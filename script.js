b0234ce1d36c2a8205a58f8e978788ec7bfa1acc
Simerdeep <simerdeepsingh567@gmail.com>
2026-02-26 00:48:12 +0530

    Gsap

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
-// gsap.to("#box1",{
-//     x:1200,
-//     rotate:360,
-//     duration:1.5,
-//     delay:1,
-// })
-
-// gsap.to("#box2",{
-//     x:1200,
-//     backgroundColor:"yellow",
-//     rotate:360,
-//     duration:1.5,
-//     delay:2.5,
-// })
-// gsap.to("#box3",{
-//     x:1200,
-//     backgroundColor:"green",
-//     scale:0.5,
-//     borderRadius:"50%",
-//     rotate:360,
-//     duration:1.5,
-//     delay:4,
-// })
-
-
-let tl = gsap.timeline()
-
-tl.to("#box1",{
-    x:1200,
-    rotate:360,
-    duration:1.5,
-    delay:1
+gsap.from("#page1 #box",{
+    scale:0,
+    duration:2,
+    delay:1,
+    rotate:360
+})
+gsap.from("#page2 #box",{
+    scale:0,
+    duration:2,
+    delay:1,
+    rotate:360
 })
 
-tl.to("#box2",{
-    x:1200,
-    rotate:360,
-    duration:1.5,
-    delay:1
-})
-tl.to("#box3",{
-    x:1200,
-    rotate:360,
-    duration:1.5,
-    delay:1
+gsap.from("#page3 #box",{
+    scale:0,
+    duration:2,
+    delay:1,
+    rotate:360
 })
