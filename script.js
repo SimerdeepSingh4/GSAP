d1c03bf68dac1a80863bfd8879e4d79019a7b3b4
Simerdeep <simerdeepsingh567@gmail.com>
2026-02-28 00:22:07 +0530

    scrollTrigger

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-gsap.from("#page1 #box",{
-    scale:0,
-    duration:2,
-    delay:1,
-    rotate:360
-})
-gsap.from("#page2 #box",{
-    scale:0,
-    duration:2,
-    delay:1,
-    rotate:360
+gsap.to("#page2 h1",{
+    transform:"translate(-190%)",
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        markers:true,
+        start:"top -10%",
+        end:"top -190%",
+        scrub:2,
+        pin:true
+    }
 })
 
-gsap.from("#page3 #box",{
-    scale:0,
-    duration:2,
-    delay:1,
-    rotate:360
-})
