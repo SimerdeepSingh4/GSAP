cfad23e807f5670fcbeaa1b3ba92f39725510d74
Simerdeep <simerdeepsingh567@gmail.com>
2026-03-05 21:37:18 +0530

    svg animation

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-gsap.to("#page2 h1",{
-    transform:"translate(-190%)",
-    scrollTrigger:{
-        trigger:"#page2",
-        scroller:"body",
-        markers:true,
-        start:"top -10%",
-        end:"top -190%",
-        scrub:2,
-        pin:true
-    }
+let initialPath =`M 10 100 Q 500 100 990 100`
+
+let finalPath =`M 10 100 Q 500 100 990 100`;
+
+let string = document.querySelector('#string')
+
+string.addEventListener('mousemove',(dets)=>{
+    path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`
+
+    gsap.to("svg path",{
+        attr:{d:path},
+        duration:0.2,
+        ease:"power3.out"
+    })
 })
 
+string.addEventListener('mouseleave',()=>{
+    gsap.to("svg path",{
+        attr:{d:finalPath},
+        duration:1,
+        ease:"elastic.out(1,0.2)"
+    })
+})
