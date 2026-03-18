397313a8fcdcb46cfa17047e2234662c299655f8
Simerdeep <simerdeepsingh567@gmail.com>
2026-03-18 23:55:36 +0530

    animated menubar using gsap timeline

--- script.js
+++ script.js
@@ -1,63 +1,24 @@
-let body = document.querySelector("body")
-let marque = document.querySelectorAll("#move .marque")
-let marque2 = document.querySelectorAll("#move2 .marque2")
+let menu = document.querySelector("#menu")
+let menuClose = document.querySelector("#menu-close")
+
+menu.addEventListener("click", function(){
+  console.log("hello")
+  let tl = gsap.timeline()
+
+tl.to("#full", {
+  right: "0",
+  duration: 1
+})
+
+tl.from("#full h4", {
+  x: 200,
+  duration: 1,
+  stagger: 0.25,
+  opacity: 0
+})
+tl.from("#full i",{
+  opacity:0
+})
+})
 
 
-
-
-  gsap.fromTo(".move", 
-      { yPercent: 0 },
-      {
-        yPercent: -50,
-        duration: 20,
-        repeat: -1,
-        ease: "none",
-      }
-    );
-
-
-  gsap.fromTo(".move2", 
-      { yPercent: -50 },
-      {
-        yPercent: 0,
-        repeat: -1,
-        duration: 20,
-        ease: "none",
-      }
-    );
-
-
-    gsap.fromTo(".move", 
-      { yPercent: 0 },
-      {
-        yPercent: -50,
-        duration: 20,
-        repeat: -1,
-        ease: "none",
-      }
-    );
-
-  const posters = [
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC1.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC19.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC9.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC18.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC4.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC11.jpg"
-  ];
-  const posters2 = [
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC14.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC7.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC11.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC28.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC13.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC23.jpg"
-  ];
-  const posters3 = [
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC18.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC3.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC11.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC22.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC1.jpg",
-    "https://ik.imagekit.io/dhyh95euj/movie%20posters/PC17.jpg"
-  ];
